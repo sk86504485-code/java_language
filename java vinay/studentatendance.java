ecc55bfa1534871fe1dd8b2048bf530c4d5aa2b7
vinay yadav <sk86504485@gmail.com>
2026-03-23 10:21:12 +0530

    Update studentatendance.java

--- java vinay/studentatendance.java
+++ java vinay/studentatendance.java
@@ -5,13 +5,17 @@ public class studentatendance {
 
     public static void main(String[] args) {
         System.out.println("\n\n\n:::::::Wellcome to Student Attendance System::::::::\n\n\n");
-        String arr[] = {"vinay", "vikas", "Divyash", "Himashu", "Satyarth", "Shivam"};
-        int totalstudent = 6;
+       System.out.print(" \n Enter total numbner of student in class  :");
         Scanner sc = new Scanner(System.in);
+        int totalstudent = sc.nextInt();
+        System.out.println(" \n\n\n:::::::::::Enter 'p' for present and 'A' for adsent student:::::\n\n\n");
+        System.out.println("\n\n::::::Student present or absent is roll number wise :::::::::\n\n\n");
+    
         int Totalpresent = 0;
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < totalstudent; i++) {
             //if student are present  than p else A;
-            System.out.println(arr[i] + "  is :");
+            System.out.print(" \n Student " + (i + 1) +"::" );
+            
             char Student = sc.next().charAt(0);
             if (Student == 'P') {
                 Totalpresent++;
@@ -22,6 +26,7 @@ public class studentatendance {
         int Totalabsent = totalstudent - Totalpresent;
         System.out.println("total absent students:" + Totalabsent);
         System.out.println("Total present students: " + Totalpresent);
+        sc.close();
     }
 
 }
